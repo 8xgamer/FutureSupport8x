27a893410b64de2c70261e2372e1851f758446e9
Silver <rautbhushan6@gmail.com>
2021-01-05 19:31:59 +0530

    added few lines

--- app/src/main/java/com/inphynous/futuresupport8x/MainActivity.java
+++ app/src/main/java/com/inphynous/futuresupport8x/MainActivity.java
@@ -1,13 +1,17 @@
 package com.inphynous.futuresupport8x;
 
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.bhushan);
+
     }
 }
